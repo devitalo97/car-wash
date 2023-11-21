c68de33825c26e4bb5c9458efdfba4d78ab3de3f
devitalo97 <dev.italo.souza@gmail.com>
2023-11-20 23:16:43 -0300

    feat(ui): add fowardRef

--- app/ui/input.tsx
+++ app/ui/input.tsx
@@ -1,48 +1,58 @@
+import React, { forwardRef, InputHTMLAttributes } from "react";
 import { ExclamationCircleIcon } from "@heroicons/react/20/solid";
 import clsx from "clsx";
 import { v4 as uuidv4 } from "uuid";
 
-interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   error?: string;
 }
 
-export default function Input({ label, error, ...rest }: Props) {
-  const id = rest?.id ?? uuidv4();
-  return (
-    <div>
-      {label ? (
-        <label
-          htmlFor={id}
-          className="block text-sm font-medium leading-6 text-white"
-        >
-          {label}
-        </label>
-      ) : null}
-      <div className="relative mt-2 rounded-md shadow-sm">
-        <input
-          {...rest}
-          id={id}
-          className={clsx(
-            "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
-            error &&
-              "pr-10 text-red-900 focus:ring-red-500 ring-red-300 placeholder:text-red-300"
-          )}
-        />
+// eslint-disable-next-line react/display-name
+const Input = forwardRef<HTMLInputElement, Props>(
+  ({ label, error, ...rest }, ref) => {
+    const id = rest?.id ?? uuidv4();
+    return (
+      <div>
+        {label ? (
+          <label
+            htmlFor={id}
+            className="block text-sm font-medium leading-6 text-white"
+          >
+            {label}
+          </label>
+        ) : null}
+        <div className="relative mt-2 rounded-md shadow-sm">
+          <input
+            ref={ref}
+            id={id}
+            aria-describedby={`error-${id}`}
+            className={clsx(
+              "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
+              error &&
+                "pr-10 text-red-900 focus:ring-red-500 ring-red-300 placeholder:text-red-300"
+            )}
+            {...rest}
+          />
+          {error ? (
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+              <ExclamationCircleIcon
+                className="h-5 w-5 text-red-500"
+                aria-hidden="true"
+              />
+            </div>
+          ) : null}
+        </div>
         {error ? (
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-            <ExclamationCircleIcon
-              className="h-5 w-5 text-red-500"
-              aria-hidden="true"
-            />
+          <div id={`error-${id}`} aria-live="polite" aria-atomic="true">
+            <p className="mt-2 text-sm text-red-600" key={error}>
+              {error}
+            </p>
           </div>
         ) : null}
       </div>
-      {error ? (
-        <p className="mt-2 text-sm text-red-600" id="email-error">
-          {error}
-        </p>
-      ) : null}
-    </div>
-  );
-}
+    );
+  }
+);
+
+export default Input;
--- app/ui/textarea.tsx
+++ app/ui/textarea.tsx
@@ -1,42 +1,49 @@
+import React, { forwardRef, TextareaHTMLAttributes } from "react";
 import clsx from "clsx";
 import { v4 as uuidv4 } from "uuid";
 
-interface Props extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+interface Props extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   label?: string;
   error?: string;
 }
 
-export default function Textarea({ label, error, ...rest }: Props) {
-  const id = rest?.id ?? uuidv4();
-  return (
-    <div>
-      {label ? (
-        <label
-          htmlFor={id}
-          className="block text-sm font-medium leading-6 text-white"
-        >
-          {label}
-        </label>
-      ) : (
-        label
-      )}
-      <div className="relative mt-2 rounded-md shadow-sm">
-        <textarea
-          {...rest}
-          rows={4}
-          id={id}
-          className={clsx(
-            "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
-            error &&
-              "pr-10 text-red-900 focus:ring-red-500 ring-red-300 placeholder:text-red-300"
-          )}
-        />
+// eslint-disable-next-line react/display-name
+const Textarea = forwardRef<HTMLTextAreaElement, Props>(
+  ({ label, error, ...rest }, ref) => {
+    const id = rest?.id ?? uuidv4();
+    return (
+      <div>
+        {label ? (
+          <label
+            htmlFor={id}
+            className="block text-sm font-medium leading-6 text-white"
+          >
+            {label}
+          </label>
+        ) : (
+          label
+        )}
+        <div className="relative mt-2 rounded-md shadow-sm">
+          <textarea
+            ref={ref}
+            rows={4}
+            id={id}
+            className={clsx(
+              "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
+              error &&
+                "pr-10 text-red-900 focus:ring-red-500 ring-red-300 placeholder:text-red-300"
+            )}
+            {...rest}
+          />
+        </div>
+        {error ? (
+          <p className="mt-2 text-sm text-red-600" id="email-error">
+            {error}
+          </p>
+        ) : null}
       </div>
-      {error ? (
-        <p className="mt-2 text-sm text-red-600" id="email-error">
-          {error}
-        </p>
-      ) : null}
-    </div>
-  );
-}
+    );
+  }
+);
+
+export default Textarea;
