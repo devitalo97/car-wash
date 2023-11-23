619600e41260b4b8fa8f5628bb406576f20a2289
devitalo97 <dev.italo.souza@gmail.com>
2023-11-22 21:16:51 -0300

    feat(auth): add client protected routes

--- auth.config.ts
+++ auth.config.ts
@@ -2,20 +2,40 @@ import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
   pages: {
-    signIn: '/admin/login',
+    signIn: '/login',
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/admin/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/admin/dashboard/order', nextUrl));
-      }
-      return true;
+      const isAdmin = isLoggedIn && auth.user.role === "admin";
+      const isLoginPage = nextUrl.pathname.startsWith("/login")
+      const isAuthRoutes =
+        nextUrl.pathname.startsWith("/dashboard") ||
+        nextUrl.pathname.startsWith("/cart") ||
+        nextUrl.pathname.startsWith("/profile")
+
+      if (!isAuthRoutes && !isLoggedIn) return true
+
+      if (isAuthRoutes && !isLoggedIn && !isLoginPage) return Response.redirect(new URL("/login", nextUrl))
+
+      if (isLoggedIn && isLoginPage && isAdmin) return Response.redirect(new URL("/dashboard", nextUrl))
+
+      if (isLoggedIn && isLoginPage && !isAdmin) return Response.redirect(new URL("/profile", nextUrl))
+
+      return true
     },
+    jwt({ token, user }) {
+      if (user) token.role = user.role
+      return token
+    },
+    session({ session, token }) {
+      // @ts-ignore
+      session.user.role = token.role
+      return session
+    }
   },
   providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
+
+
+//
