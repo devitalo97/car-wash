e6579a219fef633d41ac69ec8ce9b1985d74346c
devitalo97 <dev.italo.souza@gmail.com>
2023-12-09 15:03:51 -0300

    feat(admin/ui/calendar): add selection functionality

--- app/(admin)/ui/calendar.tsx
+++ app/(admin)/ui/calendar.tsx
@@ -1,7 +1,168 @@
+"use client";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
+import { useEffect, useState } from "react";
+
+type Day = {
+  date: string;
+  isCurrentMonth?: boolean;
+  isToday?: boolean;
+};
+
+type DayRange = {
+  start: Day | null;
+  end: Day | null;
+};
 
 export function Calendar() {
+  const [days, setDays] = useState<Day[]>([]);
+  const [selectedRange, setSelectedRange] = useState<DayRange>({
+    start: null,
+    end: null,
+  });
+
+  useEffect(() => {
+    const generateDays = () => {
+      const currentDate = new Date();
+      const year = currentDate.getFullYear();
+      const month = currentDate.getMonth() + 1; // Mês começa do zero
+      const firstDayOfMonth = new Date(year, month - 1, 1);
+      const lastDayOfMonth = new Date(year, month, 0);
+      const numberOfDays = lastDayOfMonth.getDate();
+
+      const newDays = [];
+
+      // Adiciona 5 dias do mês anterior
+      for (let i = 5; i >= 1; i--) {
+        const date = new Date(
+          year,
+          month - 2,
+          lastDayOfMonth.getDate() - i + 1
+        );
+        newDays.push({
+          date: `${year}-${(month - 1).toString().padStart(2, "0")}-${date
+            .getDate()
+            .toString()
+            .padStart(2, "0")}`,
+          isCurrentMonth: false,
+        });
+      }
+
+      // Adiciona os dias do mês atual
+      for (let i = 1; i <= numberOfDays; i++) {
+        const date = new Date(year, month - 1, i);
+        const isToday = date.toDateString() === currentDate.toDateString();
+        newDays.push({
+          date: `${year}-${month.toString().padStart(2, "0")}-${i
+            .toString()
+            .padStart(2, "0")}`,
+          isCurrentMonth: true,
+          isToday,
+        });
+      }
+
+      // Adiciona 6 dias do próximo mês
+      for (let i = 1; i <= 6; i++) {
+        const date = new Date(year, month, i);
+        newDays.push({
+          date: `${year}-${(month + 1).toString().padStart(2, "0")}-${i
+            .toString()
+            .padStart(2, "0")}`,
+          isCurrentMonth: false,
+        });
+      }
+
+      setDays(newDays);
+    };
+    generateDays();
+  }, []);
+
+  const handleDaySelection = (day: Day) => {
+    setSelectedRange((prev) => {
+      // Se ainda não há um início ou se a data selecionada é anterior à existente
+      if (!prev.start || new Date(day.date) < new Date(prev.start.date)) {
+        return { ...prev, start: day };
+      }
+
+      // Se já há um início, mas a data selecionada é após a existente
+      if (!prev.end || new Date(day.date) > new Date(prev.end.date)) {
+        return { ...prev, end: day };
+      }
+
+      // Se o usuário clicar duas vezes na mesma data inicial, defina tanto o início quanto o fim como a data clicada
+      if (
+        new Date(day.date).getTime() === new Date(prev.start.date).getTime()
+      ) {
+        return { ...prev, start: day, end: day };
+      }
+
+      // Se o usuário clicar duas vezes na mesma data final, defina tanto o início quanto o fim como a data clicada
+      if (new Date(day.date).getTime() === new Date(prev.end.date).getTime()) {
+        return { ...prev, start: day, end: day };
+      }
+
+      const distanceToStart = Math.abs(
+        new Date(day.date).getTime() - new Date(prev.start.date).getTime()
+      );
+      const distanceToEnd = Math.abs(
+        new Date(day.date).getTime() - new Date(prev.end.date).getTime()
+      );
+
+      // Se a data clicada estiver mais próxima do fim, ajuste o fim; caso contrário, ajuste o início
+      if (distanceToStart <= distanceToEnd) {
+        return { ...prev, start: day };
+      } else {
+        return { ...prev, end: day };
+      }
+    });
+  };
+
+  const isSelected = (day: Day) => {
+    if (selectedRange.start && selectedRange.end) {
+      const start = new Date(selectedRange.start.date);
+      const end = new Date(selectedRange.end.date);
+      const dayDate = new Date(day.date);
+
+      if (
+        dayDate.getTime() === start.getTime() ||
+        dayDate.getTime() === end.getTime()
+      ) {
+        return true;
+      }
+    }
+
+    if (selectedRange.start && !selectedRange.end) {
+      const start = new Date(selectedRange.start.date);
+      const dayDate = new Date(day.date);
+
+      if (dayDate.getTime() === start.getTime()) {
+        return true;
+      }
+    }
+
+    if (selectedRange.end && !selectedRange.start) {
+      const end = new Date(selectedRange.end.date);
+      const dayDate = new Date(day.date);
+
+      if (dayDate.getTime() === end.getTime()) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const isDayInRange = (day: Day) => {
+    if (selectedRange.start && selectedRange.end) {
+      const start = new Date(selectedRange.start.date);
+      const end = new Date(selectedRange.end.date);
+      const dayDate = new Date(day.date);
+
+      return start <= dayDate && dayDate <= end;
+    }
+
+    return false;
+  };
+
   return (
     <div className="mt-4 text-center lg:col-start-8 lg:col-end-13 lg:row-start-1 xl:col-start-9">
       <div className="flex items-center text-white">
@@ -33,34 +194,36 @@ export function Calendar() {
       <div className="isolate mt-2 grid grid-cols-7 gap-px rounded-lg bg-gray-200 text-sm shadow ring-1 ring-gray-200">
         {days.map((day, dayIdx) => (
           <button
+            onClick={() => handleDaySelection(day)}
             key={day.date}
             type="button"
             className={clsx(
               "py-1.5 hover:bg-gray-100 focus:z-10",
               day.isCurrentMonth ? "bg-white" : "bg-gray-50",
-              (day.isSelected || day.isToday) && "font-semibold",
-              day.isSelected && "text-white",
-              !day.isSelected &&
+              (isSelected(day) || day.isToday) && "font-semibold",
+              isSelected(day) && "text-white",
+              !isSelected(day) &&
                 day.isCurrentMonth &&
                 !day.isToday &&
                 "text-gray-900",
-              !day.isSelected &&
+              !isSelected(day) &&
                 !day.isCurrentMonth &&
                 !day.isToday &&
                 "text-gray-400",
-              day.isToday && !day.isSelected && "text-indigo-600",
+              day.isToday && !isSelected(day) && "text-indigo-600",
               dayIdx === 0 && "rounded-tl-lg",
               dayIdx === 6 && "rounded-tr-lg",
               dayIdx === days.length - 7 && "rounded-bl-lg",
-              dayIdx === days.length - 1 && "rounded-br-lg"
+              dayIdx === days.length - 1 && "rounded-br-lg",
+              isDayInRange(day) && "bg-indigo-200"
             )}
           >
             <time
               dateTime={day.date}
               className={clsx(
                 "mx-auto flex h-7 w-7 items-center justify-center rounded-full",
-                day.isSelected && day.isToday && "bg-indigo-600",
-                day.isSelected && !day.isToday && "bg-gray-900"
+                isSelected(day) && day.isToday && "bg-indigo-600",
+                isSelected(day) && !day.isToday && "bg-gray-900"
               )}
             >
               {day.date.split("-").pop()?.replace(/^0/, "")}
@@ -71,53 +234,3 @@ export function Calendar() {
     </div>
   );
 }
-
-const days: {
-  date: string;
-  isCurrentMonth?: boolean;
-  isToday?: boolean;
-  isSelected?: boolean;
-}[] = [
-  { date: "2021-12-27" },
-  { date: "2021-12-28" },
-  { date: "2021-12-29" },
-  { date: "2021-12-30" },
-  { date: "2021-12-31" },
-  { date: "2022-01-01", isCurrentMonth: true },
-  { date: "2022-01-02", isCurrentMonth: true },
-  { date: "2022-01-03", isCurrentMonth: true },
-  { date: "2022-01-04", isCurrentMonth: true },
-  { date: "2022-01-05", isCurrentMonth: true },
-  { date: "2022-01-06", isCurrentMonth: true },
-  { date: "2022-01-07", isCurrentMonth: true },
-  { date: "2022-01-08", isCurrentMonth: true },
-  { date: "2022-01-09", isCurrentMonth: true },
-  { date: "2022-01-10", isCurrentMonth: true },
-  { date: "2022-01-11", isCurrentMonth: true },
-  { date: "2022-01-12", isCurrentMonth: true, isToday: true },
-  { date: "2022-01-13", isCurrentMonth: true },
-  { date: "2022-01-14", isCurrentMonth: true },
-  { date: "2022-01-15", isCurrentMonth: true },
-  { date: "2022-01-16", isCurrentMonth: true },
-  { date: "2022-01-17", isCurrentMonth: true },
-  { date: "2022-01-18", isCurrentMonth: true },
-  { date: "2022-01-19", isCurrentMonth: true },
-  { date: "2022-01-20", isCurrentMonth: true },
-  { date: "2022-01-21", isCurrentMonth: true },
-  { date: "2022-01-22", isCurrentMonth: true, isSelected: true },
-  { date: "2022-01-23", isCurrentMonth: true },
-  { date: "2022-01-24", isCurrentMonth: true },
-  { date: "2022-01-25", isCurrentMonth: true },
-  { date: "2022-01-26", isCurrentMonth: true },
-  { date: "2022-01-27", isCurrentMonth: true },
-  { date: "2022-01-28", isCurrentMonth: true },
-  { date: "2022-01-29", isCurrentMonth: true },
-  { date: "2022-01-30", isCurrentMonth: true },
-  { date: "2022-01-31", isCurrentMonth: true },
-  { date: "2022-02-01" },
-  { date: "2022-02-02" },
-  { date: "2022-02-03" },
-  { date: "2022-02-04" },
-  { date: "2022-02-05" },
-  { date: "2022-02-06" },
-];
