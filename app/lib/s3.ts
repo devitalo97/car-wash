5a5623675384d77fb597785503d4ad583d08f846
devitalo97 <dev.italo.souza@gmail.com>
2023-12-14 14:14:56 -0300

    feat: add s3 client

---
+++ app/lib/s3.ts
@@ -0,0 +1,10 @@
+import { S3Client } from '@aws-sdk/client-s3'
+
+export const s3 = new S3Client({
+  apiVersion: '2006-03-01',
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  },
+  region: process.env.S3_REGION
+});
--- env.d.ts
+++ env.d.ts
@@ -13,5 +13,12 @@ declare namespace NodeJS {
     NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string
     STRIPE_SECRET_KEY: string
     URL: string
+    S3_ACCESS_KEY_ID: string
+    S3_SECRET_ACCESS_KEY: string
+    S3_REGION: string
+    S3_BUCKET_NAME: string
+    S3_PUBLIC_BUCKET_DIRECTORY: string
+    S3_BASE_PUBLIC_BUCKET_URL: string
+    MAX_FILE_SIZE: number
   }
 }
